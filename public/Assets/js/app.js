25a435aaa91e42774070497c5479ac24182359c9
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2022-06-20 15:23:30 +0530

    Screen Sharing System

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -119,7 +119,9 @@ var AppProcess = (function () {
       $("#videoCamOnOff").html(
         "<span class='material-icons' style='width:100%;'>videocam_off</span>"
       );
-
+      $("#ScreenShareOnOf").html(
+        '<span class="material-icons">present_to_all</span><div>Present Now</div>'
+      );
       video_st = newVideoState;
 
       removeVideoStream(rtp_vid_senders);
@@ -149,6 +151,12 @@ var AppProcess = (function () {
           },
           audio: false,
         });
+        vstream.oninactive = (e) => {
+          removeVideoStream(rtp_vid_senders);
+          $("#ScreenShareOnOf").html(
+            '<span class="material-icons ">present_to_all</span><div >Present Now</div>'
+          );
+        };
       }
       if (vstream && vstream.getVideoTracks().length > 0) {
         videoCamTrack = vstream.getVideoTracks()[0];
@@ -294,6 +302,25 @@ var AppProcess = (function () {
       }
     }
   }
+  async function closeConnection(connid) {
+    peers_connection_ids[connid] = null;
+    if (peers_connection[connid]) {
+      peers_connection[connid].close();
+      peers_connection[connid] = null;
+    }
+    if (remote_aud_stream[connid]) {
+      remote_aud_stream[connid].getTracks().forEach((t) => {
+        if (t.stop) t.stop();
+      });
+      remote_aud_stream[connid] = null;
+    }
+    if (remote_vid_stream[connid]) {
+      remote_vid_stream[connid].getTracks().forEach((t) => {
+        if (t.stop) t.stop();
+      });
+      remote_vid_stream[connid] = null;
+    }
+  }
   return {
     setNewConnection: async function (connid) {
       await setConnection(connid);
@@ -303,6 +330,9 @@ var AppProcess = (function () {
     },
     processClientFunc: async function (data, from_connid) {
       await SDPProcess(data, from_connid);
+    },
+    closeConnectionCall: async function (connid) {
+      await closeConnection(connid);
     },
   };
 })();
@@ -338,6 +368,10 @@ var MyApp = (function () {
           });
         }
       }
+    });
+    socket.on("inform_other_about_disconnected_user", function (data) {
+      $("#" + data.connId).remove();
+      AppProcess.closeConnectionCall(data.connId);
     });
     socket.on("inform_others_about_me", function (data) {
       addUser(data.other_user_id, data.connId);
