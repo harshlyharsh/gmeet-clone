6ca78125461a8ca21d22fe0c18f0036513c66605
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2022-06-20 13:31:01 +0530

    Group Audio Video Streaming

--- server.js
+++ server.js
@@ -11,4 +11,31 @@ app.use(express.static(path.join(__dirname, "")));
 var userConnections = [];
 io.on("connection", (socket) => {
   console.log("socket id is ", socket.id);
+
+  socket.on("userconnect", (data) => {
+    console.log("userconnent", data.displayName, data.meetingid);
+    var other_users = userConnections.filter(
+      (p) => p.meeting_id == data.meetingid
+    );
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meetingid,
+    });
+    var userCount = userConnections.length;
+    console.log(userCount);
+    other_users.forEach((v) => {
+      socket.to(v.connectionId).emit("inform_others_about_me", {
+        other_user_id: data.displayName,
+        connId: socket.id,
+      });
+    });
+    socket.emit("inform_me_about_other_user", other_users);
+  });
+  socket.on("SDPProcess", (data) => {
+    socket.to(data.to_connid).emit("SDPProcess", {
+      message: data.message,
+      from_connid: socket.id,
+    });
+  });
 });
